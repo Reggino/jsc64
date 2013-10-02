4cefc371f897efe196725f66f2d1e4f8ed9c8c4d
Tim de Koning <github@king-management.nl>
2013-10-02 23:31:39 +0200

    More coding standards

--- js/nl/kingsquare/as3/flash/display/BitmapData.js
+++ js/nl/kingsquare/as3/flash/display/BitmapData.js
@@ -18,44 +18,48 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-nl.kingsquare.as3.flash.display.BitmapData = Class.extend({
-    init: function(context) {
-        this.context = context;
-    },
+(function () {
+	'use strict';
 
-    fillRect: function(rect/*:Rectangle*/, color/*:uint*/) {
-    	this.context.clearRect(rect.x, rect.y, rect.width, rect.height);
-    	this.context.fillStyle = 'rgba('+((color >> 16) & 0xFF) +
-    			','+((color >> 8) & 0xFF)+','+((color >> 0) & 0xFF)+
-    			', '+((color >> 24) & 0xFF)/255+')';
-        this.context.fillRect(rect.x, rect.y, rect.width, rect.height);
-    },
+	nl.kingsquare.as3.flash.display.BitmapData = Class.extend({
+		init: function (context) {
+			this.context = context;
+		},
 
-    setPixel32: function(x/*:int*/, y/*:int*/, color/*:uint*/) {
-    	var pixelData;
+		fillRect: function (rect/*:Rectangle*/, color/*:uint*/) {
+			this.context.clearRect(rect.x, rect.y, rect.width, rect.height);
+			this.context.fillStyle = 'rgba(' + ((color >> 16) & 0xFF) +
+					',' + ((color >> 8) & 0xFF) + ',' + ((color >> 0) & 0xFF) +
+					', ' + ((color >> 24) & 0xFF) / 255 + ')';
+			this.context.fillRect(rect.x, rect.y, rect.width, rect.height);
+		},
 
-    	if (typeof color == 'undefined') color = 0;
-    	if (typeof x == 'undefined') x = 0;
-    	if (typeof y == 'undefined') y = 0;
+		setPixel32: function (x, y, color) {
+			var pixelData;
 
-		// Not all browsers implement createImageData. On such browsers we obtain the
-		// ImageData object using the getImageData method. The worst-case scenario is
-		// to create an object *similar* to the ImageData object and hope for the best
-		// luck.
-		if (this.context.createImageData) {
-			pixelData = this.context.createImageData(1, 1);
-		} else if (this.context.getImageData) {
-			pixelData = this.context.getImageData(x, y, 1, 1);
-		} else {
-			pixelData = {'width' : 1, 'height' : 1, 'data' : new Array(4)};
+			color = (color || 0);
+			x = (x || 0);
+			y = (y || 0);
+
+			// Not all browsers implement createImageData. On such browsers we obtain the
+			// ImageData object using the getImageData method. The worst-case scenario is
+			// to create an object *similar* to the ImageData object and hope for the best
+			// luck.
+			if (this.context.createImageData) {
+				pixelData = this.context.createImageData(1, 1);
+			} else if (this.context.getImageData) {
+				pixelData = this.context.getImageData(x, y, 1, 1);
+			} else {
+				pixelData = {'width' : 1, 'height' : 1, 'data' : new Array(4)};
+			}
+
+			pixelData.data[0] = (color >> 16) & 0xFF; //R
+			pixelData.data[1] = (color >> 8) & 0xFF; //G
+			pixelData.data[2] = (color) & 0xFF; //B
+			pixelData.data[3] = (color >> 24) & 0xFF; //A
+
+			// Draw the ImageData object.
+			this.context.putImageData(pixelData, x, y);
 		}
-
-		pixelData.data[0] = (color >> 16) & 0xFF //R
-		pixelData.data[1] = (color >> 8) & 0xFF //G
-		pixelData.data[2] = (color) & 0xFF //B
-		pixelData.data[3] = (color >> 24) & 0xFF //A
-
-		// Draw the ImageData object.
-		this.context.putImageData(pixelData, x, y);
-	}
-});
+	});
+}());
